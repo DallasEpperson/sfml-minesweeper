0335a6a29bc742a6d8310a5d7cb20337e3aa2b0a
Dallas Epperson <depperson@pcgus.com>
2020-02-14 23:18:10 -0500

    Initial game scaffolding

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+
+int main(){
+    //todo init window
+
+    //todo load sprites.png
+
+    //todo place bombs
+    //   _FBI has entered the chat_
+
+    //todo game loop
+
+    //todo get mouse button press
+    //  right mb == flag thing
+
+    //todo restart if explodey boi
+
+    //todo Yay if all tiles uncovered
+
+    return 0;
+}
